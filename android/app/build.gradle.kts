860f3020adc68d87f7c3232b992a6c22137abdf4
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2026-06-09 19:36:57 -0500

    Update Android Gradle, Kotlin and SDK configs
    Bump Gradle wrapper to 8.14 and upgrade Java compatibility to Java 17. Update Kotlin plugin to 2.2.20 and kotlin-stdlib to 2.3.0, move jvmTarget into a top-level kotlin { compilerOptions } block. Remove the kotlin-android plugin from the app plugins block. Set minSdk to 29 and targetSdk to 36, and add Flutter-migrator flags android.builtInKotlin=false and android.newDsl=false in gradle.properties.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,6 @@ plugins {
     id("com.google.firebase.firebase-perf")
     id("com.google.firebase.crashlytics")
     // END: FlutterFire Configuration
-    id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -24,21 +23,14 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.pmorales.omni.remote"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 29
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -94,11 +86,17 @@ android {
     }
 }
 
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+    }
+}
+
 flutter {
     source = "../.."
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.2.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.3.0")
     implementation("androidx.core:core-ktx:1.13.1")
 }
